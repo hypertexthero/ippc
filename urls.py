be372059e8fd1c4432d3d3e4a6f102f891c8e67e
Simon Griffee <simon@hypertexthero.com>
2011-12-09 14:28:13 +0100

    removing registration urls for now

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^myproject/', include('myproject.foo.urls')),
-    url(r'^accounts/', include('registration.urls')),
+    # url(r'^accounts/', include('registration.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
